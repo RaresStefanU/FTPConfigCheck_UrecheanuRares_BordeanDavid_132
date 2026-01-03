fb358fa3499d9d9b4f912ecbe950d995a76dc686
RaresStefanU <113801864+RaresStefanU@users.noreply.github.com>
2026-01-04 00:55:58 +0200

    Adaugat verificare existenta fisier

--- ftpcheck.sh
+++ ftpcheck.sh
@@ -3,8 +3,21 @@
 
 # Verifica:
 
+
 # - existenta fisierului:
 
+#daca utilizatorul nu a dat ca parametru fisierul
+if [ -z "$1" ] then
+    echo "Utilizare: $0 <cale_fisier>"
+    exit $1
+fi
+
+CALE_FISIER=$1
+
+if ! [ -e "$CALE_FISIER" ]; then
+    echo "Fisierul nu a fost gasit!"
+    exit $2
+fi
 
 # - permisiunile fisierului:
 
